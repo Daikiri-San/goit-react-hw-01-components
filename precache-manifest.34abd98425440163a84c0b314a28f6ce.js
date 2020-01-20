017d93f31b4a4b1c29492fd152ee245c0c2b668f
Nikita Holovnin <Golovninnikita@ukr.net>
2020-01-20 20:13:29 +0200

    Updates

---
+++ precache-manifest.34abd98425440163a84c0b314a28f6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac094850423aa08f252cc0c1af9ac01",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5d2b091f7c08db559fdb",
+    "url": "/goit-react-hw-01-components/static/css/main.4afc4a10.chunk.css"
+  },
+  {
+    "revision": "eea500a4a7cb2ffeb055",
+    "url": "/goit-react-hw-01-components/static/js/2.c5f50678.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-01-components/static/js/2.c5f50678.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d2b091f7c08db559fdb",
+    "url": "/goit-react-hw-01-components/static/js/main.bb5f6cc8.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
