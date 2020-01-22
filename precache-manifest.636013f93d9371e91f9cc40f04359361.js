f205d4c89c60b1efd75063ced586b1e9de7caacc
Nikita Holovnin <Golovninnikita@ukr.net>
2020-01-22 18:20:34 +0200

    Updates

---
+++ precache-manifest.636013f93d9371e91f9cc40f04359361.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ac54d500723db229184d7eb8ece069",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7088da140aec4e8ae92b",
+    "url": "/goit-react-hw-01-components/static/css/main.4fb406b5.chunk.css"
+  },
+  {
+    "revision": "eea500a4a7cb2ffeb055",
+    "url": "/goit-react-hw-01-components/static/js/2.c5f50678.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-01-components/static/js/2.c5f50678.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7088da140aec4e8ae92b",
+    "url": "/goit-react-hw-01-components/static/js/main.ea205a0a.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/goit-react-hw-01-components/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/goit-react-hw-01-components/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "8c608256fb0273e3a36e6b603f71f213",
+    "url": "/goit-react-hw-01-components/static/media/roboto-v20-latin-500.8c608256.ttf"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/goit-react-hw-01-components/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/goit-react-hw-01-components/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/goit-react-hw-01-components/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.34abd98425440163a84c0b314a28f6ce.js"
+  "/goit-react-hw-01-components/precache-manifest.636013f93d9371e91f9cc40f04359361.js"
 );
 
 self.addEventListener('message', (event) => {
